75018e5e949ad06bb7db4803d8b454ff884ace11
dahonen <daveahonen@yahoo.com>
2016-01-13 16:40:15 -0700

    Added a prompt myName.

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("How are you?");
 console.log("I am having a hard time remembering the steps.");
+var myName = prompt("What is your name?");
+if (myName = "Dave Ahonen") {
+}
+  alert("Nice to meet you Dave Ahonen");
